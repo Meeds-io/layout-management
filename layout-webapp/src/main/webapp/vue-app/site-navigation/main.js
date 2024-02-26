8232069366c133c19323858c4c368839b63b5f91
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-02-26 12:38:55 +0100

    feat: Rename Site Management Portlet - MEED-3406 - Meeds-io/MIPs#120 (#3)
    This change will rename portlets to avoid collision with old Sites
    Management. In addition, the common CSS Styles will be defined inside
    Portlet Sttyles instead of Portal Styles to avoid polluting loaded CSS
    for pages loaded by simple users.

--- layout-webapp/src/main/webapp/vue-app/site-navigation/main.js
+++ layout-webapp/src/main/webapp/vue-app/site-navigation/main.js
@@ -41,26 +41,22 @@ extensionRegistry.registerComponent('manageSpaceDrawers', 'manage-space-drawers'
   rank: 20,
 });
 
-Vue.use(Vuetify);
-const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
-
 const appId = 'siteNavigation';
 
 //getting language of the PLF
 const lang = eXo && eXo.env.portal.language || 'en';
 
 //should expose the locale ressources as REST API
-const urls = [`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.SiteNavigationPortlet-${lang}.json`];
-
-exoi18n.loadLanguageAsync(lang, urls);
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.SiteNavigation-${lang}.json`;
 
 export function init(canManageSiteNavigation) {
-  exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
-    // init Vue app when locale ressources are ready
-    Vue.createApp({
-      template: `<site-navigation id="${appId}" :can-manage-site-navigation="${canManageSiteNavigation}"/>`,
-      vuetify,
-      i18n},
-    `#${appId}`, 'site-navigation');
-  });
+  exoi18n.loadLanguageAsync(lang, url)
+    .then(i18n => {
+      // init Vue app when locale ressources are ready
+      Vue.createApp({
+        template: `<site-navigation id="${appId}" :can-manage-site-navigation="${canManageSiteNavigation}"/>`,
+        vuetify: Vue.prototype.vuetifyOptions,
+        i18n},
+      `#${appId}`, 'site-navigation');
+    });
 }
