27f2951a69b4ac42074c0e0d68b86d28895ceb52
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-03-01 15:34:16 +0100

    fix: Display Topbar Shadow in Layout Editor - MEED-3408 - Meeds-io/MIPs#120 (#6)

--- layout-webapp/src/main/webapp/vue-app/layout-editor/components/toolbar/Toolbar.vue
+++ layout-webapp/src/main/webapp/vue-app/layout-editor/components/toolbar/Toolbar.vue
@@ -20,8 +20,10 @@
 -->
 <template>
   <v-card
-    class="d-flex align-center no-border-radius px-4"
+    class="d-flex align-center no-border-radius border-box-sizing absolute-vertical-center t-0 px-4"
     height="57"
+    min-width="100vw"
+    width="100vw"
     flat>
     <v-icon class="icon-default-color">fa-pager</v-icon>
     <span class="px-2">{{ $t('layout.editPageName', {0: pageName}) }}</span>
@@ -49,6 +51,9 @@ export default {
     pageName() {
       return this.nodeLabels?.labels && (this.nodeLabels.labels[eXo.env.portal.language] || this.nodeLabels.labels[this.defaultLanguage]) || this.page?.title;
     },
-  }
+  },
+  mounted() {
+    document.querySelector('#layoutTopBarContentChildren').append(this.$el);
+  },
 };
 </script>
