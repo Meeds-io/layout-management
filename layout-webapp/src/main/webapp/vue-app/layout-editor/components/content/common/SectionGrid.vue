a21000ef4b806723737e253782d3a77f373e0ab9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-03-11 05:50:33 +0100

    feat: Display a fixed grid when moving apps - MEED-3417 - Meeds-io/MIPs#120

---
+++ layout-webapp/src/main/webapp/vue-app/layout-editor/components/content/common/SectionGrid.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    :class="gridClass"
+    class="d-grid grid-gap-cols-1 grid-gap-rows-1">
+    <div
+      v-for="i in length"
+      :key="i"
+      :id="`grid-cell-${i}`"
+      :class="cellClass"
+      class="grey-background opacity-5"></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    rows: {
+      type: Number,
+      default: null,
+    },
+    cols: {
+      type: Number,
+      default: null,
+    },
+    cellClass: {
+      type: String,
+      default: null,
+    },
+  },
+  data: () => ({
+    updateGrid: false,
+  }),
+  computed: {
+    gridClass() {
+      return `grid-cols-${this.cols} grid-rows-${this.rows}`;
+    },
+    length() {
+      return this.rows * this.cols;
+    },
+  },
+};
+</script>
