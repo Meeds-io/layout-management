Meeds-io/layout

feat: Add Dynamic section definition - MEED-4967 - Meeds-io/MIPs#120 (#13)

layout-webapp/src/main/webapp/vue-app/layout-editor/components/content/container/Cell.vue
@@ -27,13 +27,56 @@
       'z-index-two': hover && !$root.drawerOpened,
       'elevation-1': hasApplication && hover && !$root.movingCell,
     }"
+    :style="{
+      'min-height': isDynamicSection && !hasApplication && '50px' || 'initial',
+    }"
     class="position-relative d-flex flex-column"
+    no-draggable
     @hovered="hover = $event"
     @initialized="computeHasContent">
-    <template #header>
+    <template #footer>
+      <v-card
+        v-if="isDynamicSection"
+        :class="{
+          'position-relative mb-n5': hasApplication,
+          'absolute-all-center': !hasApplication,
+        }"
+        :min-height="hasApplication && 50"
+        class="position-relative d-flex align-center justify-center flex-grow-1 flex-shrink-1 z-index-one"
+        color="transparent">
+        <v-btn
+          :class="{
+            'absolute-all-center': hasApplication,
+          }"
+          :title="$t('layout.addApplicationButton')"
+          min-width="50%"
+          class="primary"
+          outlined
+          @click="$root.$emit('layout-cell-add-application', parentId, container)">
+          <v-icon color="primary">far fa-plus-square</v-icon>
+        </v-btn>
+      </v-card>
+      <div
+        v-else-if="hasApplication && !moving"
+        ref="placeholder"
+        :class="{
+          'linear-gradient-grey-background': hasContent,
+          'white': !hasContent,
+        }"
+        class="position-relative flex-grow-1 flex-shrink-1 overflow-hidden">
+        <div
+          v-if="!hasContent"
+          class="absolute-all-center d-flex flex-column align-center justify-center">
+          <v-icon size="40" class="icon-default-color mb-5">far fa-hourglass</v-icon>
+          <span class="font-weight-bold">{{ $t('layout.inSearchOfData') }}</span>
+          <span v-if="applicationTitle" class="caption">{{ applicationTitle }}</span>
+          <span v-if="applicationCategoryTitle" class="caption">({{ applicationCategoryTitle }})</span>
+        </div>
+      </div>
       <layout-editor-cell-top-menu
         v-if="hasApplication"
         :container="container"
+        :dynamic-section="isDynamicSection"
         :parent-id="parentId"
         :hover="hover"
         :moving="moving"
@@ -56,21 +99,6 @@
           }" />
       </v-hover>
     </template>
-    <template v-if="hasApplication && !moving" #footer>
-      <div
-        ref="placeholder"
-        :class="hasContent && 'linear-gradient-grey-background' || 'white'"
-        class="position-relative flex-grow-1 flex-shrink-1 overflow-hidden">
-        <div
-          v-if="!hasContent"
-          class="absolute-all-center d-flex flex-column align-center justify-center">
-          <v-icon size="40" class="icon-default-color mb-5">far fa-hourglass</v-icon>
-          <span class="font-weight-bold">{{ $t('layout.inSearchOfData') }}</span>
-          <span v-if="applicationTitle" class="caption">{{ applicationTitle }}</span>
-          <span v-if="applicationCategoryTitle" class="caption">({{ applicationCategoryTitle }})</span>
-        </div>
-      </div>
-    </template>
   </layout-editor-container-base>
 </template>
 <script>
@@ -92,6 +120,12 @@ export default {
   computed: {
     moving() {
       return this.storageId && this.$root.movingCell?.storageId === this.storageId;
+    },
+    sectionType() {
+      return this.$layoutUtils.getSection(this.$root.layout, this.parentId)?.template;
+    },
+    isDynamicSection() {
+      return this.sectionType === this.$layoutUtils.flexTemplate;
     },
     multiSelectEnabled() {
       return this.$root.isMultiSelect

Meeds-io/layout-management

layout-webapp/src/main/webapp/vue-app/layout-editor/components/content/container/Cell.vue
@@ -27,13 +27,56 @@
       'z-index-two': hover && !$root.drawerOpened,
       'elevation-1': hasApplication && hover && !$root.movingCell,
     }"
+    :style="{
+      'min-height': isDynamicSection && !hasApplication && '50px' || 'initial',
+    }"
     class="position-relative d-flex flex-column"
+    no-draggable
     @hovered="hover = $event"
     @initialized="computeHasContent">
-    <template #header>
+    <template #footer>
+      <v-card
+        v-if="isDynamicSection"
+        :class="{
+          'position-relative my-n5': hasApplication,
+          'absolute-all-center': !hasApplication,
+        }"
+        :min-height="hasApplication && 50"
+        class="position-relative d-flex align-center justify-center flex-grow-1 flex-shrink-1 z-index-one"
+        color="transparent">
+        <v-btn
+          :class="{
+            'absolute-all-center': hasApplication,
+          }"
+          :title="$t('layout.addApplicationButton')"
+          min-width="50%"
+          class="primary"
+          outlined
+          @click="$root.$emit('layout-cell-add-application', parentId, container)">
+          <v-icon color="primary">far fa-plus-square</v-icon>
+        </v-btn>
+      </v-card>
+      <div
+        v-else-if="hasApplication && !moving"
+        ref="placeholder"
+        :class="{
+          'linear-gradient-grey-background': hasContent,
+          'white': !hasContent,
+        }"
+        class="position-relative flex-grow-1 flex-shrink-1 overflow-hidden">
+        <div
+          v-if="!hasContent"
+          class="absolute-all-center d-flex flex-column align-center justify-center">
+          <v-icon size="40" class="icon-default-color mb-5">far fa-hourglass</v-icon>
+          <span class="font-weight-bold">{{ $t('layout.inSearchOfData') }}</span>
+          <span v-if="applicationTitle" class="caption">{{ applicationTitle }}</span>
+          <span v-if="applicationCategoryTitle" class="caption">({{ applicationCategoryTitle }})</span>
+        </div>
+      </div>
       <layout-editor-cell-top-menu
         v-if="hasApplication"
         :container="container"
+        :dynamic-section="isDynamicSection"
         :parent-id="parentId"
         :hover="hover"
         :moving="moving"
@@ -56,21 +99,6 @@
           }" />
       </v-hover>
     </template>
-    <template v-if="hasApplication && !moving" #footer>
-      <div
-        ref="placeholder"
-        :class="hasContent && 'linear-gradient-grey-background' || 'white'"
-        class="position-relative flex-grow-1 flex-shrink-1 overflow-hidden">
-        <div
-          v-if="!hasContent"
-          class="absolute-all-center d-flex flex-column align-center justify-center">
-          <v-icon size="40" class="icon-default-color mb-5">far fa-hourglass</v-icon>
-          <span class="font-weight-bold">{{ $t('layout.inSearchOfData') }}</span>
-          <span v-if="applicationTitle" class="caption">{{ applicationTitle }}</span>
-          <span v-if="applicationCategoryTitle" class="caption">({{ applicationCategoryTitle }})</span>
-        </div>
-      </div>
-    </template>
   </layout-editor-container-base>
 </template>
 <script>
@@ -92,6 +120,12 @@ export default {
   computed: {
     moving() {
       return this.storageId && this.$root.movingCell?.storageId === this.storageId;
+    },
+    sectionType() {
+      return this.$layoutUtils.getSection(this.$root.layout, this.parentId)?.template;
+    },
+    isDynamicSection() {
+      return this.sectionType === this.$layoutUtils.flexTemplate;
     },
     multiSelectEnabled() {
       return this.$root.isMultiSelect