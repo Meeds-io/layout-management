3eed32710caafc8ff73bde96973696487665dfa3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-05-20 13:34:41 +0100

    feat: Add Page Template Illustration Preview - MEED-6849 - Meeds-io/MIPs#133

--- layout-webapp/src/main/webapp/vue-app/page-templates-management/components/list/PageTemplateItem.vue
+++ layout-webapp/src/main/webapp/vue-app/page-templates-management/components/list/PageTemplateItem.vue
@@ -5,12 +5,8 @@
       v-if="!$root.isMobile"
       align="center"
       width="70px">
-      <div :aria-label="$t('pageTemplate.label.preview', {0: name})">
-        <v-img
-          :src="illustrationSrc"
-          max-height="30"
-          max-width="60" />
-      </div>
+      <page-templates-management-item-illustration
+        :page-template="pageTemplate" />
     </td>
     <!-- name -->
     <td
@@ -73,12 +69,6 @@ export default {
     },
     description() {
       return this.$te(this.pageTemplate?.description) ? this.$t(this.pageTemplate?.description) : this.pageTemplate?.description;
-    },
-    illustrationId() {
-      return this.pageTemplate?.illustrationId;
-    },
-    illustrationSrc() {
-      return this.illustrationId && `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/attachments/pageTemplate/${this.pageTemplateId}/${this.illustrationId}` || '/layout/images/page-templates/DefaultPreview.webp';
     },
     category() {
       const i18nKey = `layout.pageTemplate.category.${this.pageTemplate?.category}`;
