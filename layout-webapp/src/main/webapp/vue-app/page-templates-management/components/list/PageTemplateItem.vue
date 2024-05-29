396935b3378fee3fbc69a2f51eb0cd788fad8657
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-05-29 17:06:28 +0100

    fix: Fix Page Template Width - MEED-6942 - Meeds-io/meeds#2063 (#89)

--- layout-webapp/src/main/webapp/vue-app/page-templates-management/components/list/PageTemplateItem.vue
+++ layout-webapp/src/main/webapp/vue-app/page-templates-management/components/list/PageTemplateItem.vue
@@ -14,7 +14,7 @@
       align="left">
       <v-card
         v-sanitized-html="name"
-        class="text-truncate transparent"
+        class="transparent"
         flat
         @click="$root.$emit('page-templates-illustration-preview', illustrationSrc)" />
     </td>
@@ -22,7 +22,6 @@
     <td
       v-if="!$root.isMobile"
       align="left"
-      class="text-truncate"
       v-sanitized-html="description"></td>
     <td
       v-if="!$root.isMobile"
