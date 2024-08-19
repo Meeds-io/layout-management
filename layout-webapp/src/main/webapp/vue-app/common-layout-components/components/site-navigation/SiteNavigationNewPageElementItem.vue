7a3c97431f63833e94e2901c5d00022fd04f1aa8
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2024-08-19 17:58:28 +0100

    feat: Template list review - MEED-7326 - Meeds-io/MIPs#137 (#182)
    This PR allows to review the UI/UX of the template list drawer.

--- layout-webapp/src/main/webapp/vue-app/common-layout-components/components/site-navigation/SiteNavigationNewPageElementItem.vue
+++ layout-webapp/src/main/webapp/vue-app/common-layout-components/components/site-navigation/SiteNavigationNewPageElementItem.vue
@@ -19,27 +19,57 @@
 
 <template>
   <div>
-    <div
-      :style="imageContainerStyle"
-      :class="selected && 'selected'"
-      class="overflow-hidden rounded pa-1 hover-elevation card-selection"
-      @click="$emit('select')">
-      <img
-        :src="illustrationSrc"
-        :style="imageStyle"
-        class="align-center mb-5 mx-auto object-fit-cover">
-    </div>
-    <div class="text-center mt-2">
-      <p
-        v-if="title"
-        v-sanitized-html="title"
-        class="mb-0 font-weight-regular text-truncate"></p>
-      <p
-        v-if="description"
-        v-sanitized-html="description"
-        :class="!expand && 'text-truncate-2'"
-        class="mb-0 text-subtitle"></p>
-    </div>
+    <v-hover
+      v-model="hover"
+      open-delay="200"
+      close-delay="100">
+      <div
+        :style="containerStyle"
+        :class="selected && 'selected'"
+        class="d-flex flex-column rounded pa-2 hover-elevation card-selection position-relative">
+        <p
+          v-if="title"
+          v-sanitized-html="title"
+          class="mb-0 font-weight-regular text-truncate-2"></p>
+        <div class="d-flex flex-grow-1 align-center">
+          <div
+            :style="imageContainerStyle"
+            class="overflow-hidden">
+            <img
+              :src="illustrationSrc"
+              :style="imageStyle"
+              class="align-center mx-auto object-fit-cover">
+          </div>
+        </div>
+        <div
+          v-if="hover"
+          class="position-absolute full-height full-width t-0 l-0 mask-color rounded z-index-drawer d-flex flex-column pa-2 overflow-hidden">
+          <p
+            v-if="title"
+            v-sanitized-html="title"
+            class="white--text mb-1 font-weight-regular text-truncate-2"></p>
+          <p
+            v-if="description"
+            v-sanitized-html="description"
+            class="white--text mb-0 text-subtitle text-truncate-2"></p>
+          <div class="d-flex flex-grow-1 align-center justify-space-around">
+            <v-btn
+              small
+              class="btn btn-primary me-2"
+              @click="$emit('select')">
+              {{ $t('siteNavigation.label.use') }}
+            </v-btn>
+            <v-btn
+              small
+              class="btn"
+              color="white"
+              @click="$root.$emit('layout-illustration-preview', illustrationSrc)">
+              {{ $t('siteNavigation.label.templatePreview') }}
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </v-hover>
   </div>
 </template>
 <script>
@@ -58,6 +88,9 @@ export default {
       default: false
     }
   },
+  data: () => ({
+    hover: false,
+  }),
   computed: {
     title() {
       return this.pageTemplate?.name;
@@ -85,9 +118,18 @@ export default {
       return {
         'max-width': '100%',
         'min-width': '100%',
-        'min-height': '100px',
-        'max-height': '100px',
-        'height': '100px'
+        'min-height': '80px',
+        'max-height': '80px',
+        'height': '80px'
+      };
+    },
+    containerStyle() {
+      return {
+        'max-width': '100%',
+        'min-width': '100%',
+        'min-height': '150px',
+        'max-height': '150px',
+        'height': '150px'
       };
     }
   },
