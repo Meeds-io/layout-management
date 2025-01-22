b3ef47599e795f43925bbd5b772f5a31650917ac
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-01-22 13:13:00 +0100

    feat: Add Site Layout Editor Portlet - MEED-8194 - Meeds-io/MIPs#175 (#303)
    This change will initiate a new porllet to allow site layout edition.

---
+++ layout-webapp/src/main/webapp/vue-app/site-layout-editor/components/toolbar/Toolbar.vue
@@ -0,0 +1,53 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-card
+    class="d-flex align-center no-border-radius border-box-sizing absolute-vertical-center t-0 px-4"
+    height="57"
+    min-width="100vw"
+    width="100vw"
+    flat>
+    <v-icon class="icon-default-color">fa-pager</v-icon>
+    <span class="px-2">{{ $t('layout.editSiteName', {0: siteLabel}) }}</span>
+    <v-spacer />
+    <site-layout-editor-toolbar-history-buttons class="me-3" />
+    <site-layout-editor-toolbar-edit-sections-button class="me-3" />
+    <site-layout-editor-toolbar-properties-button class="me-3" />
+    <site-layout-editor-toolbar-mobile-preview-button class="me-3" />
+    <site-layout-editor-toolbar-save-button />
+  </v-card>
+</template>
+<script>
+export default {
+  computed: {
+    disabledDraft() {
+      return !this.$root.sectionHistory?.length && !this.$root.sectionRedo?.length;
+    },
+    siteLabel() {
+      return this.$root.site?.displayName;
+    },
+  },
+  mounted() {
+    document.querySelector('#layoutTopBarContentChildren').append(this.$el);
+  },
+};
+</script>
--- layout-webapp/src/main/webapp/vue-app/site-management/components/main/Menu.vue
+++ layout-webapp/src/main/webapp/vue-app/site-management/components/main/Menu.vue
@@ -7,6 +7,7 @@
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 3 of the License, or (at your option) any later version.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -38,6 +39,38 @@
       </v-btn>
     </template>
     <v-list class="pa-0" dense>
+      <v-tooltip
+        v-if="canEditSite"
+        :disabled="canEditSiteLayout"
+        bottom>
+        <template #activator="{ on, attrs }">
+          <div
+            v-on="on"
+            v-bind="attrs">
+            <v-list-item
+              :aria-label="$t('siteManagement.label.editLayout')"
+              :disabled="!canEditSiteLayout"
+              :href="editSiteLayoutLink"
+              target="_blank"
+              class="px-3">
+              <v-card
+                color="transparent"
+                min-width="15"
+                flat>
+                <v-icon
+                  :class="!canEditSiteLayout && 'disabled--text'"
+                  size="13">
+                  fa-window-maximize
+                </v-icon>
+              </v-card>
+              <v-list-item-title class="ps-2">
+                <span :class="!canEditSiteLayout && 'disabled--text'">{{ $t('siteManagement.label.editLayout') }}</span>
+              </v-list-item-title>
+            </v-list-item>
+          </div>
+        </template>
+        <span>{{ noLayoutEditTooltip }}</span>
+      </v-tooltip>
       <v-list-item
         v-if="isPortalSite && !isGlobalSite"
         :aria-label="$t('siteManagement.label.properties')"
@@ -47,15 +80,13 @@
         <v-card
           color="transparent"
           min-width="15"
-          class="me-2"
           flat>
           <v-icon size="13">
             fa fa-cog
           </v-icon>
         </v-card>
-        <v-list-item-title
-          class="subtitle-2">
-          <span class="ps-1">{{ $t('siteManagement.label.properties') }}</span>
+        <v-list-item-title class="ps-2">
+          {{ $t('siteManagement.label.properties') }}
         </v-list-item-title>
       </v-list-item>
       <v-list-item
@@ -66,15 +97,13 @@
         <v-card
           color="transparent"
           min-width="15"
-          class="me-2"
           flat>
           <v-icon size="13">
-            fas fa-sitemap
+            fas fa-project-diagram
           </v-icon>
         </v-card>
-        <v-list-item-title
-          class="subtitle-2">
-          <span class="ps-1">{{ $t('siteManagement.label.navigation') }}</span>
+        <v-list-item-title class="ps-2">
+          {{ $t('siteManagement.label.navigation') }}
         </v-list-item-title>
       </v-list-item>
       <v-list-item
@@ -84,23 +113,23 @@
         class="px-3"
         @click="$root.$emit('open-manage-permissions-drawer', site, true, true)">
         <v-card
+          class="d-flex align-center justify-center"
           color="transparent"
           min-width="15"
-          class="me-2"
           flat>
           <v-icon size="13">
             fas fa-shield-alt
           </v-icon>
         </v-card>
-        <v-list-item-title
-          class="subtitle-2">
-          <span class="ps-1">{{ $t('siteManagement.label.manageAccess') }}</span>
+        <v-list-item-title class="ps-2">
+          {{ $t('siteManagement.label.manageAccess') }}
         </v-list-item-title>
       </v-list-item>
       <v-list-item
-        dense
+        class="px-3"
         @click="duplicate">
         <v-card
+          class="d-flex align-center justify-center"
           color="transparent"
           min-width="15"
           flat>
@@ -113,9 +142,10 @@
         </v-list-item-title>
       </v-list-item>
       <v-list-item
-        dense
+        class="px-3"
         @click="saveAsTemplate">
         <v-card
+          class="d-flex align-center justify-center"
           color="transparent"
           min-width="15"
           flat>
@@ -137,9 +167,10 @@
             v-bind="attrs">
             <v-list-item
               :disabled="!canDelete"
-              dense
+              class="px-3"
               @click="$root.$emit('delete-site', site)">
               <v-card
+                class="d-flex align-center justify-center"
                 color="transparent"
                 min-width="15"
                 flat>
@@ -178,14 +209,29 @@ export default {
     isGlobalSite() {
       return this.site.name === 'global';
     },
+    isAdministrationSite() {
+      return this.site.name === 'administration';
+    },
     isPortalSite() {
       return this.site.siteType === 'PORTAL';
     },
     canEditSite() {
       return this.site.canEdit;
     },
+    canEditSiteLayout() {
+      return this.canEditSite && !this.isMetaSite && !this.isGlobalSite && !this.isAdministrationSite && !this.site.displayed;
+    },
+    siteId() {
+      return this.site.siteId;
+    },
     canDelete() {
       return this.canEditSite && this.site?.properties?.removable !== 'false';
+    },
+    editSiteLayoutLink() {
+      return this.canEditSite && `${eXo.env.portal.context}/${eXo.env.portal.portalName}/site-layout-editor?siteId=${this.siteId}`;
+    },
+    noLayoutEditTooltip() {
+      return (this.isMetaSite || this.isGlobalSite || this.isAdministrationSite) && this.$t('sites.label.system.noLayoutEdit') || this.$t('sites.label.meta.noLayoutEdit');
     },
   },
   watch: {
