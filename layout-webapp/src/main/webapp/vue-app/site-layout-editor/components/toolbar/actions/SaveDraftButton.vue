8f994081d50c6fee5ecfbf0e06a5030e1adab5d8
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-01-24 13:38:49 +0100

    feat: Add Application Properties Drawer - MEED-8199 - Meeds-io/MIPs#175 (#309)
    This change will allow to add an application in a Site Layout by
    allowing to adjust its properties and settings.

---
+++ layout-webapp/src/main/webapp/vue-app/site-layout-editor/components/toolbar/actions/SaveDraftButton.vue
@@ -0,0 +1,66 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-tooltip bottom>
+    <template #activator="{on, attrs}">
+      <v-btn
+        v-on="on"
+        v-bind="attrs"
+        :loading="loading"
+        :aria-label="$t('layout.saveDraft')"
+        class="btn me-3"
+        @click="saveDraft">
+        {{ $t('layout.saveDraft') }}
+      </v-btn>
+    </template>
+    <span>{{ $t('layout.saveSiteDraftTooltip') }}</span>
+  </v-tooltip>
+</template>
+<script>
+export default {
+  data: () => ({
+    loading: false,
+  }),
+  methods: {
+    saveDraft() {
+      this.loading = true;
+      this.$root.$on('layout-draft-saved', this.stopLoadingSuccess);
+      this.$root.$on('layout-draft-save-error', this.stopLoadingError);
+      this.$root.$emit('layout-save-draft');
+    },
+    stopLoadingSuccess() {
+      this.stopLoading(true);
+    },
+    stopLoadingError() {
+      this.stopLoading();
+    },
+    stopLoading(success) {
+      if (this.loading && success) {
+        this.$root.$emit('alert-message', this.$t('layout.siteDraftSavedSuccessfully'), 'success');
+      }
+      this.$root.$off('layout-draft-saved', this.stopLoadingSuccess);
+      this.$root.$off('layout-draft-save-error', this.stopLoadingError);
+      this.loading = false;
+    },
+  },
+};
+</script>
